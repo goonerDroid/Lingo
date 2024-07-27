15c96aee1e1aac9a3d7442abf4eed90a837438a3
williamj <williamj949@gmail.com>
2024-07-27 13:41:51 +0530

    Create skeleton UI for languages selector

--- app/src/main/java/com/sublime/lingo/presentation/ui/viewmodel/TranslationViewModel.kt
+++ app/src/main/java/com/sublime/lingo/presentation/ui/viewmodel/TranslationViewModel.kt
@@ -1,10 +1,12 @@
 package com.sublime.lingo.presentation.ui.viewmodel
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sublime.lingo.data.repository.TranslationRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,10 +16,8 @@ class TranslationViewModel
     constructor(
         private val repository: TranslationRepository,
     ) : ViewModel() {
-//        var inputText by mutableStateOf("")
-//        private var outputText by mutableStateOf("")
-//        var sourceLanguage by mutableStateOf("English")
-//        var targetLanguage by mutableStateOf("Spanish")
+        private val _translationResult = MutableStateFlow<String?>(null)
+        val translationResult: StateFlow<String?> = _translationResult.asStateFlow()
 
         fun translate(
             text: String,
@@ -28,10 +28,11 @@ class TranslationViewModel
                 val result = repository.translate(text, sourceLanguage, targetLanguage)
                 result.fold(
                     onSuccess = { translatedText ->
-//                        outputText = translatedText
+                        _translationResult.value = translatedText
                     },
                     onFailure = { error ->
-                        // Handle error
+                        // Handle error (you might want to add error handling in the UI as well)
+                        _translationResult.value = "Error: ${error.message}"
                     },
                 )
             }
