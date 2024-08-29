fb73c88d02721546192e46422cfe4f3a0341e5a4
williamj <williamj949@gmail.com>
2024-08-29 16:12:03 +0530

    Fix TranslationResponse

--- app/src/main/java/com/sublime/lingo/data/remote/model/TranslationResponse.kt
+++ app/src/main/java/com/sublime/lingo/data/remote/model/TranslationResponse.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class TranslationResponse(
-    @Json(name = "translationTxt") val translatedText: String,
+    @Json(name = "translation") val translatedText: String,
 )
